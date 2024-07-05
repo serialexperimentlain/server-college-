ba790ec007a3975f5653af013482766545f819b5
serialexperimentlain <horikitasuzune@outlook.jp>
2024-07-05 19:19:41 +0900

    [add]ブログ投稿作成画面の保存時入力チェック処理実装

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,11 +13,14 @@
             @csrf<!--CSRFトークン-->
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value={{ old('post.title')}}>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+                <!-- firstの引数にバリデーションのキーを入力することで該当キーのバリデーションエラーを取得できます -->
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
+                <p class="title__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="store"/>
         </form>
